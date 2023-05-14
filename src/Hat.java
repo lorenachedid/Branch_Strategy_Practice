28c84d585b5f4738ea72c87081d613ea3aad6b84
Lorena_Chedid <lpperez06@gmail.com>
2023-05-13 23:10:14 -0400

    Created Hat Class

---
+++ src/Hat.java
@@ -0,0 +1,14 @@
+public class Hat {
+    public static void main(String[] args) {
+
+        //testing
+
+        System.out.println("Hello");
+
+        System.out.println("It's me....");
+
+
+
+
+    }
+}
