6c43622788965a47a529caea1287a4ed407a101c
Lorena_Chedid <lpperez06@gmail.com>
2023-05-13 22:53:11 -0400

    Created Water Class

---
+++ src/Water.java
@@ -0,0 +1,12 @@
+public class Water {
+    public static void main(String[] args) {
+
+        System.out.println("We're testing these waters");
+
+        System.out.println("Make all the moves you gotta do!");
+
+
+
+
+    }
+}
